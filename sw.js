caa722b79580b74ccbb4fca110d6ae3e334aaa00
noamarad666 <noam.arad1@gmail.com>
2026-03-12 13:43:41 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,40 +1,6 @@
-const CACHE = 'kessef-v3';
-const BASE = '/my-expenses';
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(cache =>
-      cache.addAll([
-        BASE + '/',
-        BASE + '/index.html',
-        BASE + '/manifest.json',
-        BASE + '/icon.png',
-      ])
-    )
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  if (e.request.method !== 'GET') return;
-  e.respondWith(
-    caches.match(e.request).then(cached => {
-      const network = fetch(e.request).then(res => {
-        if (res && res.status === 200) {
-          caches.open(CACHE).then(c => c.put(e.request, res.clone()));
-        }
-        return res;
-      }).catch(() => cached);
-      return cached || network;
-    })
-  );
+// Unregister any previously installed service worker
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', () => {
+  self.registration.unregister();
+  self.clients.matchAll().then(clients => clients.forEach(c => c.navigate(c.url)));
 });
